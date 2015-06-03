ca8fa8a0e732809285394e00a2b5e0b870ff8b9c
unknown <dyd1928@naver.com>
2015-06-03 18:01:16 +0900

    project setting and make PC file

---
+++ PC.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:39:25 06/03/2015 
+// Design Name: 
+// Module Name:    PC 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module PC(
+    input branch,
+    output reg address
+    );
+
+	
+
+endmodule
