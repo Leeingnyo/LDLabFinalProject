55883a3420120562bed2ffafde97bb12fcb79068
Leeingnyo <dyd1928@naver.com>
2015-06-16 18:06:37 +0900

    세그먼트 위치 변경

--- pa.fromHdl.tcl
+++ pa.fromHdl.tcl
@@ -1,7 +1,7 @@
 
 # PlanAhead Launch Script for Pre-Synthesis Floorplanning, created by Project Navigator
 
-create_project -name finalProject -dir "D:/LDLabFinalProject/planAhead_run_1" -part xc3s50antqg144-4
+create_project -name finalProject -dir "C:/Users/cse/Desktop/LDLabFinalProject/planAhead_run_2" -part xc3s50antqg144-4
 set_param project.pinAheadLayout yes
 set srcset [get_property srcset [current_run -impl]]
 set_property target_constrs_file "test.ucf" [current_fileset -constrset]
